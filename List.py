74ec7837850eb874a9d58aed21de4fd0cfa08081
poornimagh <poornima@appinessworld.com>
2024-09-15 19:49:44 +0530

    15/09/24

---
+++ List.py
@@ -0,0 +1,18 @@
+# List is mutable, homogenous
+name = [ "raju","gundu","Surya"]
+
+
+#Tuple is a mutable, homogenous
+vegetable = ("carrot","beetroot","radish")
+
+#set is immutable where duplicate values are not allowed
+fruit = {"apple","orange","pear"}
+
+#dictionary is a immutable,keys will be different but values can be same.
+dict = {"name": "Vismitha" , "Native":"Shettihalli"}
+
+print("The list elements are",name)
+print("The Tuple elements are",vegetable)
+print("The set values are",fruit)
+print("The dictionary values are",dict)
+
