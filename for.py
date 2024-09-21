d7d7baa9541f45316cc19e2792990ff0cf8ead35
poornimagh <poornima@appinessworld.com>
2024-09-21 23:40:25 +0530

    for loop practice

---
+++ for.py
@@ -0,0 +1,17 @@
+x = ["Vismitha",25,"Gundu","2.5"]
+
+for i in x:
+    print(i)
+
+#example 1:
+    for i in range(10):
+        print(i)
+
+
+#example 2:
+for i in range(1,51,2):
+    print(i)
+
+#example 3 (reverse order):
+for i in range(50,1,-2):
+    print(i)
